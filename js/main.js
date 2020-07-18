fe1b397ac52b5c0410ce04629dba93ce8a3c9abd
Mukund Sadani <54810884+mukundsadani07@users.noreply.github.com>
2020-07-18 10:16:37 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+  /*---------Start Preloader---------*/
+  var overlay = document.getElementById("preloader");
+  window.addEventListener('load', function()
+  { 
+    overlay.style.display='none';
+  })
+
+
+  /* End Preloader */
+  
+  /*----------Start Nav--------*/
+  
+  $('body').scrollspy({ target: '#navbar' })
+
+  /* End Nav */
+
+
+
+	
+
+	
